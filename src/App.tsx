a51fbb453fb5be78048f29fc9c0accb420676503
Oleksii Popov <alexejpopovua@gmail.com>
2018-11-17 17:16:50 +0100

    chore(package): Reordering resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import './App.css';
-
-import logo from './logo.svg';
+import './styles/App.css';
+import logo from './resources/logo.svg';
 
 class App extends React.Component {
   public render() {
