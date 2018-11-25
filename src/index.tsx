ebce4946adba94f75e08d5cc1f3e83f552f9fa40
Oleksii Popov <alexejpopovua@gmail.com>
2018-11-25 19:10:42 +0100

    fix(styles): Build styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,4 +3,4 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './../styles/index.scss';
 
-window.addEventListener("load", () => ReactDOM.render(<App />, document.body));
+window.addEventListener("load", () => ReactDOM.render(<App/>, document.querySelector(".main") as HTMLElement));
