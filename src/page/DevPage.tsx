b6ce163f2d683dcd85cf623a83d162cbd9a588fb
Oleksii Popov <alexejpopovua@gmail.com>
2019-08-18 14:11:21 +0200

    feat(md): ability to load and highlight code in md files

--- src/page/DevPage.tsx
+++ src/page/DevPage.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import content from "./DevPageContent.md";
+
+const dangerousHtml = {__html: content};
 
 export default () => (
     <div className="page-content page-dev">
         <h1 className="w3-text-teal">Dev Page</h1>
         <div className="page-content">
             <p>Development page. Not for production!</p>
+            <div dangerouslySetInnerHTML={dangerousHtml}/>
         </div>
     </div>
 );
